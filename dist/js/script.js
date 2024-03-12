c1882c6b2e71d10374527894748d6e68e9c6f4fc
Dmytro <solonar96@gmail.com>
2024-03-12 16:02:56 +0200

    added all links and added closing function to menu links

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger'),
 	  menu = document.querySelector('.menu'),
-	  closeElem = document.querySelector('.menu__close');
+	  closeElem = document.querySelector('.menu__close'),
+	  linkElem = document.querySelector('.menu__list');
 
 hamburger.addEventListener('click', () => {
 	menu.classList.add('active');
@@ -10,9 +11,29 @@ closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
+linkElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
 const counters = document.querySelectorAll('.skills__ratings-counter'),
 	  lines = document.querySelectorAll('.skills__ratings-line span');
 
 counters.forEach( (item, i) => {
 	lines[i].style.width = item.innerHTML;
 });
+
+
+// const sidePanelLink = document.querySelectorAll('.sidepanel__link'),
+// 	  sidePanelDivider = document.querySelector('.sidepanel__divider'),
+// 	  sidePanelText = document.querySelector('.sidepanel__text'),
+// 	  initialHeight = document.documentElement.scrollTop + 560,
+// 	  heightToChange = document.documentElement.scrollHeight - 450,
+// 	  sidePanel = document.querySelector('.sidepanel');
+
+// sidePanel.addEventListener('scroll', function() {
+// 	if (Math.abs(document.documentElement.scrollTop - initialHeight) >= heightToChange) {
+// 		sidePanelLink.classList.add('sidepanel__link-black');
+// 		sidePanelDivider.classList.add('sidepanel__divider-black');
+// 		sidePanelText.classList.add('sidepanel__text-black');
+// 	}
+// });
